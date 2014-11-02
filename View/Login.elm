d557c5d0290346ffb5b5ea5a681b0822288d4821
Tom Ashworth <tashworth@twitter.com>
2014-11-02 17:05:57 +0000

    style tweaks

--- View/Login.elm
+++ View/Login.elm
@@ -12,18 +12,20 @@ import Input.Login ( loginFormInputs )
 render : Model.View
 render appState dim =
   case appState.user of
-        Nothing -> loginForm appState dim
-        Just user -> empty
+    Nothing   -> loginForm appState dim
+    Just user -> empty
 
 loginForm : Model.View
 loginForm appState (w,h) =
-  let pos = midTopAt (relative 0.5) (absolute 40) in
-  container w h pos
-    <| flow down
-      (intersperse
-        (spacer 5 5)
-        [ leftAligned (I.style I.sTitle "Login")
-        , field defaultStyle loginFormInputs.username.handle identity "Phone, email or username" appState.login.username
-        , password defaultStyle loginFormInputs.password.handle identity "Password" appState.login.password
-        , button loginFormInputs.action.handle () "Login"
-        ])
+  let
+    pos = midTopAt (relative 0.5) (absolute 40)
+  in
+    container w h pos
+      <| flow down
+      <| intersperse
+          (spacer 5 5)
+          [ leftAligned (I.style I.sTitle "Login")
+          , field defaultStyle loginFormInputs.username.handle identity "Phone, email or username" appState.login.username
+          , password defaultStyle loginFormInputs.password.handle identity "Password" appState.login.password
+          , button loginFormInputs.action.handle () "Login"
+          ]
